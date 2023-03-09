e315113136fbfcf0416e345149b570383037a043
Alexander <som17son25@gmail.com>
2023-03-09 10:49:39 +0300

    refactor: Registration and create validateRegistration

--- src/features/Registration/Registration.tsx
+++ src/features/Registration/Registration.tsx
@@ -3,19 +3,22 @@ import React from 'react'
 import { Paper, Typography } from '@mui/material'
 import { Formik } from 'formik'
 import { Navigate, NavLink } from 'react-router-dom'
-import * as Yup from 'yup'
 
 import { useAppDispatch, useAppSelector } from 'app/store'
+import { RegisterData } from 'common/api/DataTypes'
 import { PATH } from 'common/path/path'
 import { registration } from 'features/Registration/registration-reducer'
 import s from 'features/Registration/Registration.module.scss'
 import RegistrationForm from 'features/Registration/RegistrationForm/RegistrationForm'
+import { validateRegister } from 'features/Registration/validateRegistration'
 
-type Login = {}
-
-const Registration: React.FC<Login> = () => {
+const Registration = () => {
   const dispatch = useAppDispatch()
   const isRegistered = useAppSelector<boolean>(state => state.registration.isRegistered)
+
+  const submitRegisterForm = ({ email, password }: RegisterData) => {
+    dispatch(registration({ email, password }))
+  }
 
   if (isRegistered) {
     return <Navigate to={PATH.LOGIN.LOGIN} />
@@ -27,21 +30,8 @@ const Registration: React.FC<Login> = () => {
         <Typography className={s.title}>Sing Up</Typography>
         <Formik
           initialValues={{ email: '', password: '', confirmPassword: '' }}
-          // todo 'Вывести в переменную, создать константы'
-          validationSchema={Yup.object().shape({
-            email: Yup.string().email('Invalid email address').required('Required'),
-            password: Yup.string()
-              .min(7, 'Password must be at least 7 characters')
-              .required('Required'),
-            confirmPassword: Yup.string()
-              .required('Required')
-              .oneOf([Yup.ref('password')], 'Passwords must match'),
-          })}
-          // todo 'Вывести в переменную, убрать лог, прописать логику
-          onSubmit={(values, { resetForm }) => {
-            dispatch(registration({ email: values.email, password: values.password }))
-            resetForm()
-          }}
+          validationSchema={validateRegister}
+          onSubmit={submitRegisterForm}
         >
           {formik => <RegistrationForm formik={formik} />}
         </Formik>
